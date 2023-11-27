34fe1c90340dfc8428d0daa7d8563925a4be36db
demchukv <demchukv@gmail.com>
2023-11-27 10:53:20 +0200

    clear and correct

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -23,6 +23,7 @@ const photoLoader = {
     searchTerm: document.querySelector(".searchTerm"),
     loader: document.querySelector(".loader"),
     gallery: document.querySelector(".gallery"),
+    totalHits: 0,
 
     simpleGallery: new SimpleLightbox(".gallery a", {
         overlayOpacity: 0.8,
@@ -42,7 +43,7 @@ const photoLoader = {
         this.fetchPhotos();
     },
 
-    continueSearch() {
+    loadMorePhotos() {
         this.searchParams.page += 1;
         this.fetchPhotos();
     },
@@ -50,12 +51,14 @@ const photoLoader = {
     renderPhotos(photos) {
         if (photos.total === 0) {
             this.showAlert('Sorry, there are no images matching your search query. Please try again!');
-            this.showLoader(false);
+            this.showLoaderAndButtons(false);
             return;
         }
+        this.totalHits = photos.totalHits;
         const markup = photos.hits
             .map((photo) => {
-                return `<a class="gallery-link" href="${photo.largeImageURL}"><span class="gallery-item"><img class="gallery-image" src="${photo.webformatURL}" alt="${photo.tags}" title="${photo.tags}" /></span>
+                return `<a class="gallery-link" href="${photo.largeImageURL}">
+                <span class="gallery-item"><img class="gallery-image" src="${photo.webformatURL}" alt="${photo.tags}" title="${photo.tags}" /></span>
                 <span class="img-stat">
                 <span class="img-stat-item"><span class="img-stat-title">Likes</span><span class="img-stat-val">${photo.likes}</span></span>
                 <span class="img-stat-item"><span class="img-stat-title">Views</span><span class="img-stat-val">${photo.views}</span></span>
@@ -66,14 +69,6 @@ const photoLoader = {
             })
             .join("");
         this.gallery.insertAdjacentHTML("beforeend", markup);
-
-        if (this.searchParams.page === 1 && photos.totalHits > 1) {
-            getMoreBtn.style.display = 'block';
-        }        
-        if (Math.ceil(photos.totalHits / this.searchParams.per_page) === this.searchParams.page) {
-            getMoreBtn.style.display = 'none';
-            this.showAlert("We're sorry, but you've reached the end of search results.");
-        }
         this.simpleGallery.refresh();
         this.scrollTop();
     },
@@ -85,9 +80,19 @@ const photoLoader = {
         }
     },
 
-    showLoader(state = true) {
+    showLoaderAndButtons(state = true) {
         this.loader.style.display = !state ? 'none' : 'inline-block';
+        getMoreBtn.style.display = !state ? 'block' : 'none';
         this.searchPhotosBtn.disabled = state;
+        if (state === false) {
+            if (this.searchParams.page === 1 && this.totalHits > 1) {
+                getMoreBtn.style.display = 'block';
+            }
+            if (Math.ceil(this.totalHits / this.searchParams.per_page) === this.searchParams.page) {
+                getMoreBtn.style.display = 'none';
+                this.showAlert("We're sorry, but you've reached the end of search results.");
+            }
+        }
     },
 
     showAlert(msg) {
@@ -104,134 +109,17 @@ const photoLoader = {
     },
 
     async fetchPhotos() {
-        this.showLoader(true);
+        this.showLoaderAndButtons();
         try {
             const response = await axios.get(this.url, { params: this.searchParams });
-            this.renderPhotos(await response.data);
+            this.renderPhotos(response.data);
         } catch (error) {
             this.showAlert(error.message);
         }
-        this.showLoader(false);
+        this.showLoaderAndButtons(false);
     }
-
 }
 
 form.addEventListener("submit", (event) => photoLoader.startSearch(event));
 
-getMoreBtn.addEventListener("click", () => photoLoader.continueSearch());
-
-
-/*
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import alertImg from "/img/alert.svg";
-import axios from "axios";
-
-const url = "https://pixabay.com/api/";
-const searchParams = {
-    key: "40878457-91c7c8077e8a58c5727309d44",
-    q: "",
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page: 1,
-};
-
-const form = document.querySelector('.form');
-const searchTerm = document.querySelector('.searchTerm');
-const searchPhotosBtn = document.querySelector(".search-btn");
-const getMoreBtn = document.querySelector(".get-more-btn");
-const loader = document.querySelector('.loader');
-const gallery = document.querySelector(".gallery");
-
-const simpleGallery = new SimpleLightbox('.gallery a', {
-    overlayOpacity: 0.8,
-    captionsData: 'alt',
-    captionDelay: 250,
-});
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (searchTerm.value.trim().length < 3) {
-        showAlert("Please, enter search term!");
-        return;
-    }
-    gallery.innerHTML = '';
-    showLoader(true);
-    searchParams.q = searchTerm.value.trim();
-    searchParams.page = 1;
-    fetchPhotos();
-});
-
-getMoreBtn.addEventListener("click", () => {
-    searchParams.page += 1;
-    showLoader(true);
-    fetchPhotos();
-})
-
-async function fetchPhotos() {
-  try {
-    const response = await axios.get(url, { params: searchParams });
-      renderPhotos(await response.data);
-  } catch (error) {
-      showAlert(error.message);
-      showLoader(false);
-  }
-}
-
-function renderPhotos(photos) {
-    if (photos.total === 0) {
-        showAlert('Sorry, there are no images matching your search query. Please try again!');
-        showLoader(false);
-        return;
-    }
-    const markup = photos.hits
-        .map((photo) => {
-            return `<a class="gallery-link" href="${photo.largeImageURL}"><span class="gallery-item"><img class="gallery-image" src="${photo.webformatURL}" alt="${photo.tags}" title="${photo.tags}" /></span>
-            <span class="img-stat">
-            <span class="img-stat-item"><span class="img-stat-title">Likes</span><span class="img-stat-val">${photo.likes}</span></span>
-            <span class="img-stat-item"><span class="img-stat-title">Views</span><span class="img-stat-val">${photo.views}</span></span>
-            <span class="img-stat-item"><span class="img-stat-title">Comments</span><span class="img-stat-val">${photo.comments}</span></span>
-            <span class="img-stat-item"><span class="img-stat-title">Downloads</span><span class="img-stat-val">${photo.downloads}</span></span>
-            </span>
-            </a>`;
-        })
-        .join("");
-    showLoader(false);
-    gallery.insertAdjacentHTML("beforeend", markup);
-
-    if (searchParams.page === 1 && photos.totalHits > 1) {
-        getMoreBtn.style.display = 'block';
-    }        
-    if (Math.ceil(photos.totalHits / searchParams.per_page) === searchParams.page) {
-        getMoreBtn.style.display = 'none';
-        showAlert("We're sorry, but you've reached the end of search results.");
-    }
-    simpleGallery.refresh();
-    if (searchParams.page > 1) {
-        const rect = document.querySelector(".gallery-link").getBoundingClientRect();
-        window.scrollBy({ top: rect.height * 2, left: 0, behavior: "smooth" });
-    }
-}
-
-function showLoader(state = true) {
-    loader.style.display = !state ? 'none' : 'inline-block';
-    searchPhotosBtn.disabled = state;
-}
-
-function showAlert(msg) {
-    iziToast.show({
-        position: 'center',
-        iconUrl: alertImg,
-        messageColor: '#FAFAFB',
-        messageSize: '16px',
-        backgroundColor: '#EF4040',
-        close: false,
-        closeOnClick: true,
-        message: msg
-    });
-}
-*/
+getMoreBtn.addEventListener("click", () => photoLoader.loadMorePhotos());
